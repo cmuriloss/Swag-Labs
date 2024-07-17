e70f84e1d29b2ca7187e6ba43c525d3bb9238dee
cmuriloss <smurilo.ti@gmail.com>
2024-07-17 22:49:03 +0100

    alteração no commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,6 @@
 Cypress.Commands.add('loginCredential', function(){
-    cy.get('[data-test="username"]').type("standard_user")
-    cy.get('[data-test="password"]').type("secret_sauce")
+    cy.get('[data-test="username"]').type("standard_user").should("have.value", "standard_user")
+    cy.get('[data-test="password"]').type("secret_sauce").should("have.value", "secret_sauce")
     cy.get('[data-test="login-button"]').click()
        
 })
